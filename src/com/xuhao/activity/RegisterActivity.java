6195e8ff646494f366c3486d96a1e3345b5a1f10
nikkyhao <930334832@qq.com>
2015-08-11 17:16:55 +0800

    完善了注册界面，但是有问题，获取的字符都是空的。

--- src/com/xuhao/activity/RegisterActivity.java
+++ src/com/xuhao/activity/RegisterActivity.java
@@ -1,14 +1,23 @@
 package com.xuhao.activity;
 
+import java.net.PasswordAuthentication;
+
 import cn.bmob.v3.listener.InitListener;
+import cn.bmob.v3.listener.SaveListener;
 
 import com.example.androidtestproject.R;
+import com.xuhao.javaBean.User;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.ContactsContract.CommonDataKinds.Nickname;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends Activity{
     EditText mid,mpsd,msex,mname,musertype;
@@ -30,14 +39,42 @@ public class RegisterActivity extends Activity{
 	musertype=(EditText)findViewById(R.id.register_usertype);
 	btn_register=(TextView)findViewById(R.id.register_next);
     }
-    private void init() {
-  	// TODO Auto-generated method stub
-  	
-	
-	
-	
-	
-	
-	
-      }
+
+	private void init() {
+		btn_register.setOnClickListener(new MyRegisterListener());
+
+	}
+    
+    class MyRegisterListener implements OnClickListener{
+    	String userid = mid.getText().toString();
+    	String password = mpsd.getText().toString();
+    	String sex = msex.getText().toString();
+    	String Nickname = mname.getText().toString();
+    	String idnetity = musertype.getText().toString();
+		@Override
+		public void onClick(View v) {
+			Log.d("userid", userid);
+			
+			
+			User user = new User();
+			user.setUserName(userid);
+			user.setPassword(password);
+			user.setSex(sex);
+			user.setNickName(Nickname);
+			user.setIdentity(idnetity);
+			user.save(RegisterActivity.this, new SaveListener() {
+				@Override
+				public void onSuccess() {
+					Toast toast = Toast.makeText(RegisterActivity.this, "注册成功", Toast.LENGTH_SHORT);
+				 	toast.show();
+				}
+				@Override
+				public void onFailure(int arg0, String arg1) {
+					Toast toast = Toast.makeText(RegisterActivity.this, "注册失败"+arg1, Toast.LENGTH_SHORT);
+				 	toast.show();
+				}
+			});
+		}
+    	
+    }
 }
