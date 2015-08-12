e27c52c72d2928a13cda8be809ea932e90ed0ee2
930334832@qq.com <930334832@qq.com>
2015-08-12 14:17:46 +0800

    添加了用户中心

--- src/com/xuhao/activity/NearByFragment.java
+++ src/com/xuhao/activity/NearByFragment.java
@@ -1,10 +1,29 @@
 package com.xuhao.activity;
 
+
+import com.example.androidtestproject.R;
+
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by ritaa on 2015/8/1.
  */
 public class NearByFragment extends Fragment{
-
+    View mBaseView;
+    private Context mcontext;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreateView(inflater, container, savedInstanceState);
+        mcontext=getActivity();
+        mBaseView=inflater.inflate(R.layout.fragement_usercenter,null);
+        
+        return mBaseView;
+    }
 }
